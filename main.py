b3a5056317c9229bf0f0ac7d1b9a66c5e91d5c6d
steinholtzt <t.steinholtz@gmail.com>
2019-12-09 10:34:15 +0100

    Adding Main

---
+++ main.py
@@ -0,0 +1,85 @@
+# importing libraries 
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt
+   
+from particle_filter import ParticleFilter
+
+
+
+#Partile Filter Parameters
+N = 100
+
+
+i = 1
+
+
+#While loop Parameters
+captured = False
+
+
+# Create a VideoCapture object and read from input file 
+cap = cv2.VideoCapture('Cocacola.mp4')
+# Check if camera opened successfully 
+if (cap.isOpened()== False):  
+  print("Error opening video  file") 
+   
+
+
+
+# Read until video is completed 
+while(cap.isOpened()): 
+      
+  # Capture frame-by-frame 
+    ret, frame = cap.read()
+    max_x, max_y, z = np.shape(frame)
+    particle_frame = frame
+
+    if ret == True:
+        if captured == True:
+            s = PF.predict(s, max_x, max_y)
+            particle_frame[s[0,:], s[1,:] ] = [0, 255, 0]
+            
+            testar = PF.update(s, sq[3]/2, sq[2]/2, frame)
+
+
+
+        dst = cv2.addWeighted(frame,0.5,particle_frame,0.5,0)
+        cv2.imshow('Frame', dst)
+
+  # Press C
+        if cv2.waitKey(25) & 0xFF == ord('c'):
+            captured = True
+            sq = cv2.selectROI('Choose object', frame)
+
+            imag_mask = frame[sq[1]:sq[1]+sq[3], sq[0]:sq[0]+sq[2], :]
+            hist_r, bins_r = np.histogram(imag_mask[:,:,0], bins=50, range=(0, 255), density=True)
+            hist_b, bins_b = np.histogram(imag_mask[:,:,1], bins=50, range=(0, 255), density=True)
+            hist_g, bins_b = np.histogram(imag_mask[:,:,2], bins=50, range=(0, 255), density=True)
+            print("Histogram made")
+
+            PF = ParticleFilter(N, hist_r, max_x, max_y)
+            s, weights = PF.state_init(max_x, max_y)
+            particle_frame[s[0,:], s[1,:] ] = [0, 255, 0]
+            # plt.subplot(231), plt.plot(hist_r), plt.title('Red')
+            # plt.subplot(232), plt.plot(hist_g), plt.title('Green')
+            # plt.subplot(233), plt.plot(hist_b), plt.title('Blue')
+            # plt.show()
+
+
+
+
+        # Press Q on keyboard to  exit 
+        if cv2.waitKey(25) & 0xFF == ord('q'): 
+            break
+    
+  # Break the loop 
+    else:  
+        break
+   
+# When everything done, release  
+# the video capture object 
+cap.release() 
+   
+# Closes all the frames 
+cv2.destroyAllWindows()
